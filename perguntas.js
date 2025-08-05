951cd8fe11ae23ea9c430edc3639765867435a43
renatagds <grose.santos@escola.pr.gov.br>
2025-08-05 15:14:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual o nome da árvore que representa o sul do Brasil?',
+    'Araucária ou pinheiro do Paraná'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é JavaScript?',
+    'JavaScript é uma liguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    '26 estados e um destrito federal'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 144?',
+    'É 12 a raiz quadrada de 144'
+)
